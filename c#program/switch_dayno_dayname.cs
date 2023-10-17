20b8c76d06bbc62c3affc1f62f61e01ee499add8
Gitchetan@1234 <chetanraut1251@gmail.com>
2023-10-17 22:40:02 +0530

    c#program

---
+++ c#program/switch_dayno_dayname.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_dayno_and_dayname
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayno;
+            Console.WriteLine("enter dayno between 1 to 7");
+            dayno = Convert.ToInt32(Console.ReadLine());
+            switch (dayno)
+            {
+                case 1:
+                    Console.WriteLine("monday");
+                    break;
+                case 2:
+                    Console.WriteLine("tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("fryday");
+                    break;
+                case 6:
+                    Console.WriteLine("saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("sunday");
+                    break;
+                default:
+                    Console.WriteLine("invalid day");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
