77222a61222946045455a3f690385be29cc03a60
Gitchetan@1234 <chetanraut1251@gmail.com>
2023-10-21 14:37:14 +0530

    windowsfiles

---
+++ windowsfiles/equalno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_equalno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            if (num1 == num2)
+                label3.Text = "no is equal";
+            else
+                label3.Text = "no is not equal";
+        }
+    }
+}
