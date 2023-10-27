813f64e2e88f40fb839fe3cd97ad24dcc6b4ebfd
Gitchetan@1234 <chetanraut1251@gmail.com>
2023-10-27 18:06:16 +0530

    c#program

---
+++ c#program/arr_5submark.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arr_5submark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = new int[5];
+            int total = 0;
+            float per = 0;
+            string grade = null;
+            for(int cnt=0;cnt<5;cnt++)
+            {
+                Console.WriteLine("enter 5 sub marks");
+                marks[cnt] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            for(int cnt=0;cnt<5;cnt++)
+            {
+                total = total + marks[cnt];
+                per = (total / 500.0f) * 100.0f;
+
+            }
+            
+            if(per>90)
+            {
+                grade = "distinction";
+
+            }
+            else if(per>80 && per<90)
+            {
+                grade = "first class";
+            }
+            else if(per>60 && per<80)
+            {
+                grade = "second class";
+            }
+            else
+            {
+                grade = "failed";
+            }
+            Console.WriteLine("total : " + total);
+            Console.WriteLine("per: " + per);
+            Console.WriteLine("grade: " + grade);
+            Console.ReadKey();
+
+      
+        }
+    }
+}
---
+++ c#program/arr_findelemenetinarr.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arr_findelementinarr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5, };
+            int mila = 0;
+            int username;
+            Console.WriteLine("enter number to be searched");
+            username = Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<num.Length;i++)
+            {
+                if (num[i]==username)
+                {
+                    mila = 1;
+                    break;
+
+                }
+            }
+            if(mila==1)
+            {
+                Console.WriteLine("mum{0} is found in an array", username);
+
+            }
+            else
+            {
+                Console.WriteLine("number is not found in an array");
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
