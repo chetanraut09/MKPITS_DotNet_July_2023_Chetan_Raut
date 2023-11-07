0fc3e1920908cf8190760db21b8944cd64a6c1da
Gitchetan@1234 <chetanraut1251@gmail.com>
2023-11-07 17:38:05 +0530

    c#program

---
+++ c#program/destructor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex0f
+{
+    class Employee
+    {
+        //creating a constructor
+        public Employee()
+        {
+            Console.WriteLine("constructor called");
+
+        }
+        ~Employee()
+        {
+            Console.WriteLine("destructer called");
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            Console.ReadKey();
+            Employee emp1= new Employee();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c#program/method_ol_withdiffparameter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_ol_withdiffparameter
+{
+    class Calculate
+    {
+        public void addition(int n1,int n2)
+        {
+            int res=n1 + n2;
+            Console.WriteLine("addition of two int is:"+res);
+        }
+        public void addition(float n1,float n2)
+        {
+            float res=n1 + n2;
+            Console.WriteLine("addition of two float:" + res);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate c= new Calculate();
+            c.addition(1, 2);
+            Console.WriteLine("now calling addition of float no");
+            c.addition(2.3f, 3.2f);
+            Console.ReadKey();
+
+        }
+    }
+}
