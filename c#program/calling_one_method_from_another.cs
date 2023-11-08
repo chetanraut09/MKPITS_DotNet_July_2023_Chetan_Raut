78028faf7c1786d56cf3a734abc82bd1063d11c9
Gitchetan@1234 <chetanraut1251@gmail.com>
2023-11-08 17:32:01 +0530

    c#program

---
+++ c#program/calling_one_method_from_another.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calling_one_method_from_another
+{
+    class book
+    {
+        string title, author;
+
+        public void getdata(string title)
+        {
+            Console.WriteLine("method with one parameter");
+            getdata(title, "mansi");  //calling method with 2 parameters
+        }
+        public void getdata(string title, string author)
+        {
+            Console.WriteLine("method with 2 parameter");
+            this.title = title;
+            this.author = author;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("title : " + title);
+            Console.WriteLine("author : " + author);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            book b1 = new book();
+            b1.getdata("html");
+            b1.display();
+            Console.ReadKey();
+        }
+    }
+}
