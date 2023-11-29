39ddc2f46bdabe1b057a4139be718cab955fcb0d
Gitchetan@1234 <chetanraut1251@gmail.com>
2023-11-29 18:04:58 +0530

    dll_progfram

---
+++ dll_progfram/catch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int n;
+            Console.WriteLine("enter number1");
+            num=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number2");
+            n=Convert.ToInt32(Console.ReadLine());
+
+            int res = 0;
+            try
+            {
+                res = num / n; //error will come here and program will terminate
+
+            }
+            catch (Exception e) 
+            {
+                
+                Console.WriteLine("cannot divided by zero");
+
+            }
+            Console.WriteLine("res=" + res);
+            Console.WriteLine("bye");
+
+        }
+    }
+}
---
+++ dll_progfram/finally.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @finally
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int n;
+            Console.WriteLine("enter number1");
+            num=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number2");
+            n= Convert.ToInt32(Console.ReadLine());
+
+            int res = 0;
+            try
+            {
+                res = num / n; //error will come here and terminate the program
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("catch block is executing");
+                Console.WriteLine("cannot devided by zero");
+            }
+            finally
+            {
+                Console.WriteLine("finally block is executing");
+                Console.WriteLine("res=" + res);
+            }
+            Console.WriteLine("bye");
+        }
+    }
+}
