8e3e7d6a53c4701d753549d056423e1acb4eee81
Gitchetan@1234 <chetanraut1251@gmail.com>
2023-12-04 20:44:34 +0530

    windowsfiles

---
+++ windowsfiles/checkbox_salary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checkbox_salary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int bsal=Convert.ToInt32(button1.Text);
+            float bonus = bsal + 0.40f;
+            float totalsalary = bsal + bonus;
+            
+            if(checkBox1.Checked)
+            {
+                label3.Text = "bonus:" + bonus;
+            }
+            if(checkBox2.Checked)
+            {
+                label4.Text = "total salary:" + totalsalary;
+
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ windowsfiles/sumof5no_arr.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sumof5no_arr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] num= new int[5];
+        int i= 0;
+        int sum = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(i<5)
+            {
+                num[i] = Convert.ToInt32(textBox1.Text);
+                sum = sum + i;
+                i++;
+                textBox1.Clear();
+                textBox1.Focus();
+
+                if(i==5)
+                {
+                    label2.Text = "sum of 5 no:" + sum;
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
