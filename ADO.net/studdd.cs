c86afc71cd485637a14241b11a55b0ea7cbca600
Gitchetan@1234 <chetanraut1251@gmail.com>
2024-01-03 22:43:51 +0530

    ADO.net

---
+++ ADO.net/studdd.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace studdd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void cleartext()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            comboBox1.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            studddclass s = new studddclass();
+            string g = null;
+            if (radioButton1.Checked)
+            {
+                g = "male";
+
+            }
+            if (radioButton2.Checked)
+            {
+                g = "femail";
+            }
+
+            string h = null;
+            if (checkBox1.Checked)
+            {
+                h = "music";
+
+            }
+            if (checkBox2.Checked)
+            {
+                if (h != null)
+                {
+                    h = h + " " + "reading";
+                }
+                else
+                {
+                    h = "reading";
+                }
+            }
+            string result = s.InserStudent(Convert.ToInt32(textBox1.Text), textBox2.Text, g, h, comboBox1.Text, dateTimePicker1.Value.ToShortDateString());
+            label7.Text = result;
+            cleartext();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            studddclass s = new studddclass();
+            string g = null;
+            if (radioButton1.Checked)
+            {
+                g = "male";
+
+            }
+            if (radioButton2.Checked)
+            {
+                g = "femail";
+            }
+
+            string h = null;
+            if (checkBox1.Checked)
+            {
+                h = "music";
+
+            }
+            if (checkBox2.Checked)
+            {
+                if (h != null)
+                {
+                    h = h + " " + "reading";
+                }
+                else
+                {
+                    h = "reading";
+                }
+            }
+            string result = s.UpdateStudent(Convert.ToInt32(textBox1.Text), textBox2.Text, g, h, comboBox1.Text, dateTimePicker1.Value.ToShortDateString());
+            label7.Text = result;
+            cleartext();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            studddclass s = new studddclass();
+            string g = null;
+            if (radioButton1.Checked)
+            {
+                g = "male";
+
+            }
+            if (radioButton2.Checked)
+            {
+                g = "femail";
+            }
+
+            string h = null;
+            if (checkBox1.Checked)
+            {
+                h = "music";
+
+            }
+            if (checkBox2.Checked)
+            {
+                if (h != null)
+                {
+                    h = h + " " + "reading";
+                }
+                else
+                {
+                    h = "reading";
+                }
+            }
+            string result = s.DeleteStudent(Convert.ToInt32(textBox1.Text), textBox2.Text);
+            label7.Text = result;
+            cleartext();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            private void button4_Click(object sender, EventArgs e)
+            {
+                List<Student> li = new List<Student>();
+
+                StudentForm stud = new StudentForm();
+                //RollNo
+                li = stud.SearchStudentDetails(Convert.ToInt32(textBox1.Text));
+
+                //StudentName
+                textBox2.Text = li[0].StudentName;
+
+                //Gender
+                string Gender = li[0].Gender;
+
+                if (Gender == "Male")
+                {
+                    radioButton1.Checked = true;
+
+                }
+                if (Gender == "Female")
+                {
+                    radioButton2.Checked = true;
+                }
+
+                //hobby
+                string Hobby = li[0].Hobby;
+
+                if (Hobby == "Reading" && Hobby == "Music")
+                {
+                    checkBox1.Checked = true;
+                    checkBox2.Checked = true;
+                }
+                if (Hobby == "Reading")
+                {
+                    checkBox1.Checked = true;
+                }
+                if (Hobby == "Music")
+                {
+                    checkBox2.Checked = true;
+                }
+
+                //City
+                comboBox1.Text = li[0].City;
+
+                //DOB
+                dateTimePicker1.Text = li[0].DOB;
+            }
+
+        }
+    }
---
+++ ADO.net/studddclass.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace studdd
+{
+    class student
+    {
+        public int rno { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string hobby { get; set; }
+        public string city { get; set; }
+        public string dob { get; set; }
+
+        public student(int rno, string name, string gender, string hobby, string city, string dob)
+        {
+            this.rno = rno;
+            this.name = name;
+            this.gender = gender;
+            this.hobby = hobby;
+            this.city = city;
+            this.dob = dob;
+        }
+    }
+
+
+
+        class studddclass
+    {
+        string strcon = "server=TNL21885300-B\\SQLEXPRESS;integrated " +
+            "security=true;database=studdd;";
+        //creating a method to insert record in student table
+
+        public string InserStudent (int rno , string name , string gender , string hobby, string city , string dob)
+        {
+            using(SqlConnection con = new SqlConnection(strcon))
+            {
+                con.Open ();
+                string str = "insert into studdd values(@rno,@name ,@gender, @hobby , @city ,@dob)";
+
+                using (SqlCommand command=new SqlCommand (str,con)) 
+                {
+                    command.Parameters.AddWithValue ("@rno", rno);
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@gender", gender);
+                    command.Parameters.AddWithValue("@hobby", hobby);
+                    command.Parameters.AddWithValue("@city", city);
+                    command.Parameters.AddWithValue("@dob", dob);
+
+                    command.ExecuteNonQuery();
+                    con.Close ();
+                    MessageBox.Show("add");
+                    return "record save successfully";
+                }
+            }
+        }
+
+        public string UpdateStudent (int rno, string name, string gender, string hobby, string city, string dob)
+        {
+            using (SqlConnection con = new SqlConnection(strcon))
+            {
+                con.Open();
+                string str = "Update studdd set name=@name,gender=@gender,hobby=@hobby,city=@city,dob=@dob where rno=@rno";
+
+                using (SqlCommand command = new SqlCommand(str, con))
+                {
+                   
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@gender", gender);
+                    command.Parameters.AddWithValue("@hobby", hobby);
+                    command.Parameters.AddWithValue("@city", city);
+                    command.Parameters.AddWithValue("@dob", dob);
+                    command.Parameters.AddWithValue("@rno", rno);
+
+                    command.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("update");
+                    return "record update successfully";
+                }
+            }
+        }
+
+        public string DeleteStudent(int rno, string name)
+        {
+            using (SqlConnection con = new SqlConnection(strcon))
+            {
+                con.Open();
+                string str = "delete from studdd where rno=@rno or  name=@name";
+
+                using (SqlCommand command = new SqlCommand(str, con))
+                {
+                    command.Parameters.AddWithValue("@rno", rno);
+                    command.Parameters.AddWithValue("@name", name);
+
+
+
+                    command.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Delete");
+                    return "record Delete successfully";
+                }
+            }
+        }
+
+        public List<student> searchstudent(int rno)
+        {
+            using (SqlConnection con = new SqlConnection(strcon))
+            {
+                con.Open();
+                string str = "select *from studdd where rno=@rno ";
+
+                using (SqlCommand command = new SqlCommand(str, con))
+                {
+                    command.Parameters.AddWithValue("@rno", rno);
+                    SqlDataReader reader;
+                    reader=command.ExecuteReader();
+
+                    List<student> li=new List<student>();   
+
+                    while (reader.Read())
+                    {
+                        li.Add(new student(Convert.ToInt32(reader["rno"].ToString() ), reader["name"].ToString(),
+                            reader["gender"].ToString(), reader["hobby"].ToString(), reader["city"].ToString(),
+                            reader["dob"].ToString()));
+                    }
+                  reader.Close();
+                  return li;
+                    con.Close() ;
+
+                }
+            }
+        }
+    }
+}                                                                                                                                                                                   
